ecbc9bfb4ba3ce76908ca20739bb1141c7005fa3
QusaiTA <tashtoshqusai@gmail.com>
2021-09-27 16:45:42 +0300

    dice function solved

---
+++ basiclibrary/lib/src/main/java/lab2/Main.java
@@ -0,0 +1,11 @@
+package lab2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Word Test");
+        Library demo = new Library();
+        System.out.println(Arrays.toString(demo.roll(5)));
+    }
+}
